ff3cd3bedcf279421141e48ac957872bf435e3e5
d.sosinsky <d.sosinsky@duotek.ru>
2022-04-22 20:19:40 +0300

    attempting to fix font bug 2

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-    chainWebpack: (config) => {
-        config.plugin('html').tap((args) => {
-            args[0].minify = {
-                ...args[0].minify,
-                removeAttributeQuotes: false,
-            }
-            return args
-        })
-    }
+    
 };
